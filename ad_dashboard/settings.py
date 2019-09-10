77bbdb316956feb6a726c4cfe9c64c683defb81d
Przemysław Palacz <pprzemal@gmail.com>
2019-09-10 15:51:05 +0200

    Install ad_statistics app

--- ad_dashboard/settings.py
+++ ad_dashboard/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ad_statistics.apps.AdStatisticsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
