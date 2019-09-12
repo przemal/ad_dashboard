af9d6f6bfcd409ced634f6b0577e2f8f573371fe
Przemysław Palacz <pprzemal@gmail.com>
2019-09-12 18:12:54 +0200

    Add basic SPA serving

--- ad_dashboard/settings.py
+++ ad_dashboard/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'ad_dashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend', 'build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static')
+]
 
 
 # Cross-Origin Resource Sharing (CORS) policy
--- ad_dashboard/urls.py
+++ ad_dashboard/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.views.generic import TemplateView
 
 from ad_statistics.urls import urlpatterns as ad_statistics_urls
 
@@ -23,4 +24,5 @@ from ad_statistics.urls import urlpatterns as ad_statistics_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(ad_statistics_urls)),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
